a787caf7f185d7cf474fb15b4569d001d2bdfe9e
tmcdo1 <tmcdonald1234@yahoo.com>
2016-01-18 11:23:28 -0600

    Changed control references

--- src/org/discobots/stronghold/commands/drive/TankDriveCommand.java
+++ src/org/discobots/stronghold/commands/drive/TankDriveCommand.java
@@ -21,7 +21,7 @@ public class TankDriveCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrainSub.tankDriveRamp(Robot.oi.getRawAnalogStickBLX(), Robot.oi.getRawAnalogStickBRX());    	 
+    	Robot.driveTrainSub.tankDriveRamp(Robot.oi.getRawAnalogStickALX(), Robot.oi.getRawAnalogStickARX());    	 
     }
 
     // Make this return true when this Command no longer needs to run execute()
