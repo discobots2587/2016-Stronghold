fa50a5405949604e8de632a3d52582eb48d9ac0b
sgogula100 <sgogula100@S026-5CG54024KB.student.HISD.ORG>
2016-02-06 13:43:56 -0600

    Set motor speed to 0 when move arm command ends

--- src/org/discobots/stronghold/commands/arm/MoveArmCommand.java
+++ src/org/discobots/stronghold/commands/arm/MoveArmCommand.java
@@ -41,6 +41,7 @@ public class MoveArmCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.armSub.armMotor.set(0);
     }
 
     // Called when another command which requires one or more of the same
