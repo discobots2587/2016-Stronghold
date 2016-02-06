9d1b5ef7f4154b3b54e197e7f67356a5530bc4bd
tmcdo1 <tmcdonald1234@yahoo.com>
2016-02-06 10:57:17 -0600

    Made new ArmSubsystem and added two commands for it

--- src/org/discobots/stronghold/subsystems/ArmSubsystem.java
+++ src/org/discobots/stronghold/subsystems/ArmSubsystem.java
@@ -1,44 +1,18 @@
 package org.discobots.stronghold.subsystems;
 
-import org.discobots.stronghold.HW;
-
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
-public class ArmSubsystem extends PIDSubsystem {
-private AnalogInput potentometer = new AnalogInput(HW.potentiometer);//new potentiometer
-private static final double kP = .25;
-private static final double kI = 0,kD = 0;
-private Talon armMotor = new Talon()
-PIDcontroller ArmPID=new PIDController(kP,kI,kD,potentometer,); 
-    // Initialize your subsystem here
-    public ArmSubsystem() {
-    	super(kP,kI,kD);
-    	ArmPID.setInputRange(0,5); //sets voltage from 0-5V
+public class ArmSubsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
 
-        // Use these to get going:
-        // setSetpoint() -  Sets where the PID controller should move the system
-        //                  to
-        // enable() - Enables the PID controller.b g	
-    }
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
-    
-    protected double returnPIDInput() {
-        // Return your input value for the PID loop
-        // e.g. a sensor, like a potentiometer:
-        // yourPot.getAverageVoltage() / kYourMaxVoltage;
-    	return 0.0;
-    }
-    
-    protected void usePIDOutput(double output) {
-        // Use output to drive your system, like a motor
-        // e.g. yourMotor.set(output);
-    }
 }
+
