70cac6f4bde9b48bc6e27b8f2e8f4b89e97ed86f
tmcdo1 <tmcdonald1234@yahoo.com>
2016-02-06 11:03:48 -0600

    Added motor and potentiometer to ArmSub

--- src/org/discobots/stronghold/subsystems/ArmSubsystem.java
+++ src/org/discobots/stronghold/subsystems/ArmSubsystem.java
@@ -1,5 +1,9 @@
 package org.discobots.stronghold.subsystems;
 
+import org.discobots.stronghold.HW;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,6 +13,15 @@ public class ArmSubsystem extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	public static AnalogInput potentiometer;
+	private static Talon armMotor;
+	
+	
+	public ArmSubsystem()
+	{
+		potentiometer = new AnalogInput(HW.potentiometer);
+		armMotor = new Talon(HW.armMotor);
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
