3d9254809dcc0cc99aa35b031b3823652a91c9c9
mason2smart <mason2smart@gmail.com>
2016-02-06 10:50:46 -0600

    More PID

--- src/org/discobots/stronghold/subsystems/ArmSubsystem.java
+++ src/org/discobots/stronghold/subsystems/ArmSubsystem.java
@@ -3,6 +3,7 @@ package org.discobots.stronghold.subsystems;
 import org.discobots.stronghold.HW;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
@@ -12,15 +13,18 @@ public class ArmSubsystem extends PIDSubsystem {
 private AnalogInput potentometer = new AnalogInput(HW.potentiometer);//new potentiometer
 private static final double kP = .25;
 private static final double kI = 0,kD = 0;
+private Talon armMotor = new Talon()
+PIDcontroller ArmPID=new PIDController(kP,kI,kD,potentometer,); 
     // Initialize your subsystem here
     public ArmSubsystem() {
     	super(kP,kI,kD);
+    	ArmPID.setInputRange(0,5); //sets voltage from 0-5V
+
         // Use these to get going:
         // setSetpoint() -  Sets where the PID controller should move the system
         //                  to
-        // enable() - Enables the PID controller.
+        // enable() - Enables the PID controller.b g	
     }
-    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
