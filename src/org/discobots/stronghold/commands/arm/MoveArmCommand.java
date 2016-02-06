43f2977a36fb5a4e38e05b4a80822f481ae07973
sgogula100 <sgogula100@S026-5CG54024KB.student.HISD.ORG>
2016-02-06 11:10:14 -0600

    Implemented move arm command

--- src/org/discobots/stronghold/commands/arm/MoveArmCommand.java
+++ src/org/discobots/stronghold/commands/arm/MoveArmCommand.java
@@ -9,15 +9,19 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class MoveArmCommand extends Command {
 
-    public MoveArmCommand() {
+	private double speed;
+	
+    public MoveArmCommand(double s) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.armSub);
+    	
+    	speed = s;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.armSub
+    	Robot.armSub.armMotor.set(speed);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -36,5 +40,6 @@ public class MoveArmCommand extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
