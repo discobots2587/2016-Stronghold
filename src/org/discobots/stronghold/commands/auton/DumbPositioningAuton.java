85ee2583efc4f9f048bce9893a444597c6ad4d0d
Mason <mason2smart@gmail.com>
2016-02-21 12:52:48 -0600

    working on auton
    added stages to update on driver dashboard

--- src/org/discobots/stronghold/commands/auton/DumbPositioningAuton.java
+++ src/org/discobots/stronghold/commands/auton/DumbPositioningAuton.java
@@ -1,5 +1,6 @@
 package org.discobots.stronghold.commands.auton;
 
+import org.discobots.stronghold.commands.auton.subcommands.AutonomousArcadeDrive;
 import org.discobots.stronghold.commands.auton.subcommands.WaitCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -8,11 +9,20 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class DumbPositioningAuton extends CommandGroup {
-    
+    String stage="none";
     public  DumbPositioningAuton() {
-    	addSequential();
+    	stage=("1: forwards");
+    	addSequential(new AutonomousArcadeDrive(.75,0,2000));//forwards 3/4 speed for 2 seconds until hits wall
+    	stage=("wait");
     	addSequential(new WaitCommand(500));
-        // Add Commands here:
+    	stage=("2: backing up");
+    	addSequential(new AutonomousArcadeDrive(-.4,-.4,2000));//back up while turning towards goal
+    	stage = ("wait");
+    	addSequential(new WaitCommand(250));
+    	stage = ("3: Towards Goal");
+    	addSequential(new AutonomousArcadeDrive(0,.4,1000));//forwards while turning towards goal
+    	stage = ("complete");
+    	// Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
         // these will run in order.
